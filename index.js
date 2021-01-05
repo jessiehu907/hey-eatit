08983cf6cb55dc829ceed27c7e186588caf98da6
Jessie <jessie.hu@airabbi.com>
2021-01-05 15:17:24 +0800

    add more info

--- index.js
+++ index.js
@@ -10,22 +10,49 @@ var bot = linebot({
 var items = Array(
   '鍋燒麵',
   '麥味登',
-  '咖哩',
+  '咖食堂',
   '拉亞',
   '便利商店',
   '泡麵',
-  '減肥'
+  '減肥',
+  '麻小面',
+  '銀記',
+  '八方雲集',
+  '馬祖麵館'
 );
+
+var endWord = Array(
+  '吧',
+  '啦',
+  '阿',
+  '啊',
+  '呀',
+  '咩',
+  '齁',
+  '囉',
+  '嘛'
+);
+
+var marks = Array(
+  '!',
+  '~',
+  '!!',
+  '~~',
+  '..'
+);
+
 bot.on('message', function (event) {
   if (event.message.type = 'text') {
     var msg = event.message.text;
     var item = items[Math.floor(Math.random() * items.length)];
+    var endWord = endWord[Math.floor(Math.random() * endWord.length)];
+    var marks = marks[Math.floor(Math.random() * marks.length)];
     if (msg == '吃啥') {
       if (item == '減肥') {
         event.reply(['還吃阿,減肥啦!']).then(function (data) { });
       }
       else {
-        event.reply(['吃' + item + '吧']).then(function (data) { });
+        event.reply(['吃' + item + endWord + marks]).then(function (data) { });
       }
     }
   }
